961a520f4ea22d73dab965ea003142801c469c6a
Muslim Ismailov <ismailov_mr@mail.ru>
2020-05-18 13:00:01 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Global_Intern.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
