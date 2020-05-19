309eecebf3cceab4a65f4a59e9e3b12df15a0e1c
haritjangwal3 <haritjangwal89@gmail.com>
2020-05-19 16:24:29 +1200

    Models added

---
+++ Models/Employer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Publisher.Models
+{
+    public class Employer
+    {
+        public int EmployerId {get; set;}
+        public string EmployerFirstName { get; set; }
+        public string EmployerLastName { get; set; }
+        public string EmployerCompany { get; set; }
+        public string EmployerEmail { get; set; }
+        public string EmployerPassword { get; set; }
+        public string EmployerPhone { get; set; }
+        public bool EmployerApproved { get; set; } // approved by Admin
+        public List<Internship> Internships { get; set; } 
+    }
+}
---
+++ Models/InternStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Publisher.Models
+{
+    public class InternStudent
+    {
+        public int InternStudentId { get; set; }
+        public Student Student { get; set; }
+        public Internship Internship { get; set; }
+        public List<Message> Messages { get; set; }
+    }
+}
