01ae756e33203fd6f95cd9cd447e1e92272f826c
ashlyvalsalan <64385395+ashlyvalsalan@users.noreply.github.com>
2020-05-20 13:15:57 +1200

    Models VisaStatus,EmployerCompany and StudentExperience added

---
+++ Models/EmployerCompany.cs
@@ -0,0 +1,17 @@
+﻿using Publisher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Global_Intern.Models
+{
+    public class EmployerCompany
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Info { get; set; }
+        public Employer Employer { get; set; }
+    }
+}
