d9fcafb4496184c8383de204ef4a7da24e3f483a
Muslim Ismailov <ismailov_mr@mail.ru>
2020-06-14 07:58:24 +1200

    Update 14/06/2020 8am

--- Controllers/DashboardStudentController.cs
+++ Controllers/DashboardStudentController.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Session;
-using Newtonsoft.Json.Linq;
-using System.Web;
 
 namespace Global_Intern.Controllers
 {
@@ -15,9 +9,10 @@ namespace Global_Intern.Controllers
         public DashboardStudentController()
         {
             var usr = HttpContext.Session.GetString("UserSession");
+
             if(usr != null)
             {
-                
+
             }
         }
         public IActionResult Index()
