91d6dc8470b5e79995ef6ff8f11bf4e519274b66
Muslim Ismailov <ismailov_mr@mail.ru>
2020-06-17 09:50:39 +1200

    Update 3 - 17/06/2020 10am

--- Util/JwtAuth.cs
+++ Util/JwtAuth.cs
@@ -1,12 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.IdentityModel.Tokens;
 using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Global_Intern.Util
 {
