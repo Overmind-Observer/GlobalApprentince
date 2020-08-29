d41a1b587f9d4a54c3af7fb0619b5155f1f29162
liuxin96 <50728615+liuxin96@users.noreply.github.com>
2020-08-29 17:13:51 +1200

    Create DashboardAdmin Folder

---
+++ Views/DashboardAdmin/Admin.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Global_Intern.Views.DashboardAdmin
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+       
+}
