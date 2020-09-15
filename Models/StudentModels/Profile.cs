581c351c96ad0b546362231e4d479073405805eb
DevelopedCountriesUnion <50728615+DevelopedCountriesUnion@users.noreply.github.com>
2020-09-15 17:07:39 +1200

    Update Profile.cs

--- Models/StudentModels/Profile.cs
+++ Models/StudentModels/Profile.cs
@@ -7,14 +7,14 @@ namespace Global_Intern.Models.StudentModels
 {
     public class InternStudent
     {
-        public string UserStudentType { get; set; } //Domestic, International
-        public string UserWorkingRight { get; set; } // yes, no
-        public string UserVisaType { get; set; }  //Student visa, Open-work visa, others
-        public string UserVisaExpire { get; set; } //2020/12
-        public string UserIndustryCertificates { get; set; } //CCNA 1
-        public string UserDriverType { get; set; } //Learner
-        public string UserDob { get; set; } //1994
-        public string UserEthnic { get; set; } //Asian
+        public string StudentType { get; set; } //Domestic, International
+        public string StudentWorkingRight { get; set; } // yes, no
+        public string StudentVisaType { get; set; }  //Student visa, Open-work visa, others
+        public string StudentVisaExpire { get; set; } //2020/12
+        public string StudentIndustryCertificates { get; set; } //CCNA 1
+        public string StudentDriverType { get; set; } //Learner
+        public string StudentDob { get; set; } //1994
+        public string StudentEthnic { get; set; } //Asian
 
     }
 }
