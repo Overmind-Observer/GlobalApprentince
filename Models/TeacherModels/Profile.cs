0f4e34780bf065b75abf626242ecc4aaa5a64bc9
DevelopedCountriesUnion <50728615+DevelopedCountriesUnion@users.noreply.github.com>
2020-09-16 16:49:46 +1200

    Update
    Updating Teacher model.

--- Models/TeacherModels/Profile.cs
+++ Models/TeacherModels/Profile.cs
@@ -1,3 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Global_Intern.Models.TeacherModels
+{
+    public class CourseTeacher
+    {
+        public string TeacherFirstName { get; set; } //John
+        public string TeacherLastName { get; set; }  // Liu
+
+        public string TeacherPosition { get; set; } //Architect & Engineer
+        public string TeacherDetails { get; set; }  //More than 10 years expeience as ASP.NET core developer at God IT.
+        public string TeacherEmail { get; set; } 
+     
+    }
+
+}
 
 
