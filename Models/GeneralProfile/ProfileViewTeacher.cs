47f7d57a74691e734758235f481ef2a80c5addbe
DevelopedCountriesUnion <50728615+DevelopedCountriesUnion@users.noreply.github.com>
2020-09-24 17:48:10 +1200

    Create
    Creating "ProfileViewTeacher"

---
+++ Models/GeneralProfile/ProfileViewTeacher.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+
+namespace Global_Intern.Models.GeneralProfile
+{
+    public class ProfileViewTeacher
+    {
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserAddress { get; set; } //CP
+        public string UserCity { get; set; } //CP
+        public string UserState { get; set; } //CP
+        public string UserCountry { get; set; } //CP
+        public int UserZip { get; set; }
+        public string UserPhone { get; set; }
+        public string UserImageName { get; set; }
+        public IFormFile UserImage { get; set; } // From POST
+        public string UserGender { get; set; }
+
+        public ProfileViewTeacher(User user)
+        {
+            UserFirstName = user.UserFirstName;
+            UserLastName = user.UserLastName;
+            UserAddress = user.UserAddress;
+            UserCity = user.UserCity;
+            UserState = user.UserState;
+            UserCountry = user.UserCountry;
+            UserZip = user.UserZip;
+            UserPhone = user.UserPhone;
+            UserImageName = user.UserImage;
+            UserGender = user.UserGender;
+        }
+
+    }
+
+}
