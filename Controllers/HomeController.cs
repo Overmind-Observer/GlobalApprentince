1de2d65c68b301d40ea09c1b1072136e45a78e26
Bavy Tess <bavythom@gmail.com>
2020-10-05 18:33:06 +1300

    Modified button size. Added links to navigate to pages  like About and ContactUs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -219,6 +219,11 @@ namespace Global_Intern.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
