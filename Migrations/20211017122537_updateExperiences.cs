87b3dd231155305bf49a824669d8d43dcff1e98a
sms154 <68546983+sms154@users.noreply.github.com>
2021-10-19 20:06:41 +1300

    Added CRUD functionality to Experience section

---
+++ Migrations/20211017122537_updateExperiences.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Global_Intern.Migrations
+{
+    public partial class updateExperiences : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExperienceLocation",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExperienceCompany",
+                table: "Experiences",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienceLocation",
+                table: "Experiences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExperienceCompany",
+                table: "Experiences",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- Models/Experience.cs
+++ Models/Experience.cs
@@ -2,15 +2,38 @@
 
 namespace Global_Intern.Models
 {
-    public class Experience
-    {
-        public int ExperienceId { get; set; }
-        public string ExperienceTitle { get; set; }
-        public int ExperienceCompany { get; set; }
-        public int ExperienceLocation { get; set; }
-        public DateTime ExperienceStartDate { get; set; }
-        public DateTime ExperienceEndDate { get; set; } // If Still working is true this field will go empty
-        public bool ExperienceStillWorking { get; set; }
-        public virtual User User { get; set; }
-    }
+	public class Experience
+	{
+		public int ExperienceId { get; set; }
+		public string ExperienceTitle { get; set; }
+		public string ExperienceCompany { get; set; }
+		public string ExperienceLocation { get; set; }
+		public DateTime ExperienceStartDate { get; set; }
+		public DateTime ExperienceEndDate { get; set; } // If Still working is true this field will go empty
+		public bool ExperienceStillWorking { get; set; }
+		public virtual User User { get; set; }
+
+
+		public void AddNewExperience(Experience NewExperience, User user)
+		{
+			this.ExperienceTitle = NewExperience.ExperienceTitle;
+			this.ExperienceCompany = NewExperience.ExperienceCompany;
+			this.ExperienceLocation = NewExperience.ExperienceLocation;
+			this.ExperienceStartDate = NewExperience.ExperienceStartDate;
+			this.ExperienceEndDate = NewExperience.ExperienceEndDate;
+			this.ExperienceStillWorking = NewExperience.ExperienceStillWorking;
+			this.User = user;
+		}
+
+		public Experience UpdateExperience(Experience experience, Experience UpdatedExperience)
+		{
+			experience.ExperienceTitle = UpdatedExperience.ExperienceTitle;
+			experience.ExperienceCompany = UpdatedExperience.ExperienceCompany;
+			experience.ExperienceLocation = UpdatedExperience.ExperienceLocation;
+			experience.ExperienceStartDate = UpdatedExperience.ExperienceStartDate;
+			experience.ExperienceEndDate = UpdatedExperience.ExperienceEndDate;
+			experience.ExperienceStillWorking = UpdatedExperience.ExperienceStillWorking;
+			return experience;
+		}
+	}
 }
