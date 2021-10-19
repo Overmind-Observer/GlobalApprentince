2a75bf91da2eeacb4ec6ebc559985105d2a431cb
sms154 <68546983+sms154@users.noreply.github.com>
2021-10-19 20:07:32 +1300

    Added skills section

--- Models/Skill.cs
+++ Models/Skill.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace Global_Intern.Models
 {
-    public class Skill
-    {
-        public int SkillID { get; set; }
-        public string SkillName { get; set; }
-        public string SkillLevel { get; set; }
-        public StudentInternProfile Intern { get; set; }
-    }
+	public class Skill
+	{
+		public int SkillID { get; set; }
+		public string SkillName { get; set; }
+		public string SkillLevel { get; set; }
+		public StudentInternProfile Intern { get; set; }
+
+		public void AddNewSkill(Skill NewSkill, StudentInternProfile intern)
+		{
+			this.SkillName = NewSkill.SkillName;
+			this.SkillLevel = NewSkill.SkillLevel;
+			this.Intern = intern;
+
+		}
+	}
 }
